e994225102ae285243e6b455f9d783fa1f0d3dbb
NarentherMS <Narentherms@gmail.com>
2024-02-16 15:35:10 +0530

    New changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Narenther MS',
+    author_email= 'msnarenther@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
